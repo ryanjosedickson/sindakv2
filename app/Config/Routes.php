48e7d8067e04c899201387909853b83d62de60ec
ryanjosedickson <ryanjosedickson01@gmail.com>
2026-09-16 08:43:24 +0700

    adjust routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,7 +3,7 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::index');
 
 // ============================================
 // AUTH ROUTES
